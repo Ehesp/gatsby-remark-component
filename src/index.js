1c50fd73642af6d015cd7b47804cfe2f6d4bfe17
Elliot Hesp <elliot.hesp@gmail.com>
2018-10-03 10:20:56 +0100

    Cleanup file, remove logging

--- src/index.js
+++ src/index.js
@@ -1,34 +1,32 @@
 const visit = require("unist-util-visit")
 import { html } from "./html-tags.js"
 
-module.exports = ({ markdownAST }, { components }) => {
-  if (!components || components.length == 0) {
-    setParentForNonHtmlElements(markdownAST)
-    return
-  }
-  if (components.length > 0) {
-    setParentForComponents(markdownAST, components)
-  }
-
-  function setParentForComponents(markdownAST, components) {
+function setParentForComponents(markdownAST, components) {
     for (let i = 0, len = components.length; i < len; i++) {
-      visit(markdownAST, `html`, (node, index, parent) => {
-        if (node.value == `<${components[i]}>`) {
-          parent.type = "div"
-        }
-      })
+        visit(markdownAST, `html`, (node, index, parent) => {
+            if (node.value === `<${components[i]}>`) {
+                parent.type = "div"
+            }
+        })
     }
-  }
+}
 
-  function setParentForNonHtmlElements(markdownAST) {
+function setParentForNonHtmlElements(markdownAST) {
     visit(markdownAST, "html", (node, index, parent) => {
-      if (
-        !html.some(
-          tag => node.value == `<${tag}>` || node.value.startsWith(`<${tag} `)
-        )
-      ) {
-        parent.type = "div"
-      }
+        if (
+            !html.some(
+                tag => node.value == `<${tag}>` || node.value.startsWith(`<${tag} `)
+            )
+        ) {
+            parent.type = "div"
+        }
     })
-  }
+}
+
+module.exports = ({ markdownAST }, { components = [] }) => {
+    if (!components.length) {
+        return setParentForNonHtmlElements(markdownAST)
+    }
+
+    return setParentForComponents(markdownAST, components)
 }
